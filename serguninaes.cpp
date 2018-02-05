f3be043573d63262331cdbbe9336fc045c54f4c3
Сергунина Екатерина С <serguninaese@lab227.local>
2018-02-05 09:02:23 +0300

    Hello

---
+++ serguninaes.cpp
@@ -0,0 +1,80 @@
+#include "serguninaes.h"
+
+/**
+ * Введение в дисциплину
+ */
+void serguninaes::lab1()
+{
+
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void serguninaes::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void serguninaes::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void serguninaes::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void serguninaes::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void serguninaes::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void serguninaes::lab7()
+{
+
+}
+
+
+void serguninaes::lab8()
+{
+
+}
+
+
+std::string ivanovii::get_name()
+{
+  return "Sergunina E.S.";
+}
