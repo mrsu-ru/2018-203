4a59c4291a36956b3e397a2741aa1c449212621f
Shcherbakov Dmitry <shcherbakov_dmitry@bk.ru>
2018-02-05 14:09:21 +0300

    modified:   scherbakovdv.cpp

--- scherbakovdv.cpp
+++ scherbakovdv.cpp
@@ -14,7 +14,46 @@ void scherbakovdv::lab1()
  */
 void scherbakovdv::lab2()
 {
-
+	//A[i][i] - матрица, b[i] - столбец свободных членов
+	memcpy(x,b)
+	for (int i = 0; i < N; i++)
+		{
+			if (!A[i][i])
+			{
+				double* MaxRow = A[i];
+				for (int j = i + 1; j < N; j++)
+					if (A[j][i] > MaxRow[i])
+					{
+						double* tmp = MaxRow;
+						MaxRow = A[j];
+						A[j] = tmp;
+					}
+				if (!MaxRow[i])
+					return;
+			}
+			for (int j = N - 1; j > i; j--)
+			{
+				A[i][j] /= A[i][i];
+				for (int k = i + 1; k < N; k++)
+					A[k][j] -= A[k][i] * A[i][j];
+			}
+			b[i] /= A[i][i];
+			A[i][i] = 1;
+			for (int j = N - 1; j > i; j--)
+			{
+				b[j] -= b[i] * A[j][i];
+				A[j][i] = 0;
+			}
+		}
+		for (int i = N - 1; i >= 0; i--)
+		{
+			for (int j = i - 1; j >= 0; j--)
+			{
+				b[j] -= b[i] * A[j][i];
+				A[j][i] = 0;
+			}
+		}
+		memcpy(x,b,sizeof(double)*N);
 }
 
 
