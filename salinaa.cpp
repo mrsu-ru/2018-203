bf7f9b00a0dbd90771c33433c0780ee5c3e33dce
salinaaa <35986735+salinaaa@users.noreply.github.com>
2018-02-21 09:12:02 +0300

    salin
    salin

--- salinaa.cpp
+++ salinaa.cpp
@@ -5,7 +5,7 @@
  */
 void salinaa::lab1()
 {
-
+std::cout<<"Hello world";
 }
 
 
@@ -14,7 +14,62 @@ void salinaa::lab1()
  */
 void salinaa::lab2()
 {
+double *x, max;
+	int k, index;
+	const double eps = 0.00001;
+	x = new double[N];
+	k = 0;
+	while (k < N)
+	{
+		// Поиск строки с максимальным a[i][k]
+		max = abs(A[k][k]);
+		index = k;
+		for (int i = k + 1; i < N; i++)
+		{
+			if (abs(A[i][k]) > max)
+			{
+				max = abs(A[i][k]);
+				index = i;
+			}
+		}
+		// Перестановка строк
+		if (max < eps)
+		{
+			// нет ненулевых диагональных элементов
+			cout << "Решение получить невозможно из-за нулевого столбца ";
+			cout << index << " матрицы A" << endl;
+			return 0;
+		}
+		for (int j = 0; j < N; j++)
+		{
+		    swap(A[k][j], A[index][j]);
 
+		}
+		swap(b[k],b[index]);
+
+
+		for (int i = k; i < N; i++)
+		{
+			double temp = A[i][k];
+			if (abs(temp) < eps) continue; // для нулевого коэффициента пропустить
+			for (int j = 0; j < N; j++)
+				A[i][j] = A[i][j] / temp;
+			b[i] = b[i] / temp;
+			if (i == k)  continue; // уравнение не вычитать само из себя
+			for (int j = 0; j < N; j++)
+				A[i][j] = A[i][j] - A[k][j];
+			b[i] = b[i] - b[k];
+		}
+		k++;
+	}
+	// обратная подстановка
+	for (k = N - 1; k >= 0; k--)
+	{
+		x[k] = b[k];
+		for (int i = 0; i < k; i++)
+			b[i] = b[i] - A[i][k] * x[k];
+	}
+	return x;
 }
 
 
