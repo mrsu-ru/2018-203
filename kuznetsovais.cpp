87d059175677201e05a726d97cf6cf1c2a87dad7
Кузнецова Ирина С <kuznetsovais@lab227.local>
2018-04-02 08:32:50 +0300

    1

--- kuznetsovais.cpp
+++ kuznetsovais.cpp
@@ -14,6 +14,45 @@ void kuznetsovais::lab1()
  */
 void kuznetsovais::lab2()
 {
+int i, j, k, l;
+
+    for(i=0;i<N;i++)
+        {
+          k=i;
+          for(l=i+1; l<N; l++)
+          if (abs(A[l][i])>abs(A[k][i]))
+                k=l;
+
+          swap(A[k],A[i]);
+          swap(b[k],b[i]);
+           b[i]/=A[i][i];
+          for(j=N-1; j>i; A[i][j--]/=A[i][i]);
+
+          A[i][i]=1;
+
+          for(int j=i+1; j<N;j++){
+          for(l=N-1;l>i;l--)
+          A[j][l]-=A[i][l]*A[j][i];
+          b[j]-=b[i]*A[j][i];
+
+          A[j][i]=0;
+          }
+
+            cout<<endl;
+
+          }
+          x[N-1]=b[N-1];
+          for ( int i = N - 2; i >= 0; i-- )
+         {
+           x[i] = b[i];
+           for ( int j = i + 1; j < N; j++ ) {
+              x[i] -= A[i][j] * x[j];
+              }
+              }
+
+
+        return 1;
+        }
 
 }
 
@@ -24,7 +63,22 @@ void kuznetsovais::lab2()
  */
 void kuznetsovais::lab3()
 {
+double *C = new double[N];
+double *B = new double[N];
+int z;
 
+ C[0]=A[0][1]/(-A[0][0]);
+ B[0]=b[0]/A[0][0];
+
+for(int i=1;i<N;i++)
+    {
+
+     C[i] = A[i][i+1]/(-A[i][i-1]*C[i-1]-A[i][i]);
+     B[i] = (-b[i] + A[i][i-1]*B[i-1])/(-A[i][i-1]*C[i-1]-A[i][i]);
+
+    }
+for(int i=N-1;i>=0;i--)
+    x[i]=C[i]*x[i+1]+B[i];
 }
 
 
