1c509a6c956b18c5c253defa6ba6d771d42b4cb0
Michael Nefedov <35986795+CombatLynx@users.noreply.github.com>
2018-04-11 08:13:51 +0300

    Метод простых итераций и решение нелинейных уравнений
    nefedovms

--- nefedovms.cpp
+++ nefedovms.cpp
@@ -109,7 +109,38 @@ double Q = 0;
  */
 void nefedovms::lab4()
 {
+double Eps=1e-15;
+double Err;
+double *nx = new double[N];
+for (int i=0;i<N;i++)
+	x[i]=b[i];
+int step=0;
+	
+do{
+step++;
+  for(int i=0;i < N;i++)
+  {
+   nx[i]=-b[i];
+ 
+   for(int j=0;j < N;j++)
+   {
+    if(i!=j)
+     nx[i]+=A[i][j]*x[j];
+   }
+ 
+   nx[i]/=-A[i][i];
+  }
+  Err=0;
+for(int i=0; i<N; i++) { 
+if(std::abs(x[i]-nx[i]) > Err)
+Err = std::abs(x[i]-nx[i]);
+}
+for(int i=0; i<N; i++) 
+	x[i]=nx[i];
+std::cout<<step<<"     "<<Err<<endl;
+}while (Err>Eps);
 
+delete[] nx;
 }
 
 
@@ -152,6 +183,28 @@ void nefedovms::lab8()
 void nefedovms::lab9()
 {
 
+}
+
+/** 
+* Решение нелинейных уравнений 
+*/ 
+
+void nefedovms::lab10() 
+{ 
+double y; 
+for(int i=0; i < 100; i++) { 
+double xd; 
+double eps = 1e-5; 
+y = i; 
+int ind = 0; 
+do { 
+ind++; 
+xd = y; 
+y = exp((-y)); 
+} while (abs(xd - y) > eps || ind > 1000); 
+if (y == y) break; 
+} 
+cout << y << endl; 
 }
 
 
