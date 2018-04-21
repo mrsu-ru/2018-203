157f269a57ccc52af1a0e187fb058096615f9b68
Вельмискина Арина В <velmiskinaav@lab227.local>
2018-04-21 13:53:59 +0300

    1

--- velmiskinaav.cpp
+++ velmiskinaav.cpp
@@ -62,6 +62,7 @@ void velmiskinaav::lab3()
 {
  int i;
  double znam;
+ double *x=new double[N];
 
     b[0] /= A[0][0];//Q[1]
     A[0][1] /= -A[0][0];//P[1]
@@ -79,7 +80,7 @@ void velmiskinaav::lab3()
         //обратный ход
     for(i = N - 2; i > -1; i--)
     {
-        b[i] += b[i + 1] * A[i][i + 1];
+       x[i] = b[i] + b[i + 1] * A[i][i + 1];
     }
 }
 
@@ -90,27 +91,131 @@ void velmiskinaav::lab3()
  */
 void velmiskinaav::lab4()
 {
+double norma; //чебышевская норма вектора
+ double xn[N]={0};//вектор для текущей итерации, начальное значение
+       //должно быть равно начальному приближению
+double *x=new double[N];
 
+
+ for(int i=0; i < N;i++)
+  {
+   x[i]=-b[i];
+
+   for(int j=0;j < N;j++)
+   {
+    if(i!=j)
+     x[i]+=A[i][j]*x[j];
+   }
+
+   x[i]/=-A[i][i];
+  }
+
+  for(int i=0;i < N;i++)
+  {
+   if(fabs(x[i]-xn[i]) > norma)
+    norma=fabs(x[i]-xn[i]);
+   xn[i]=x[i];
+  }
 }
 
 
 
 /**
- * Метод Якоби или Зейделя
+ * Метод Якоби
  */
 void velmiskinaav::lab5()
 {
+double eps =0.00001;
 
+		double* Y = new double[N];
+		double norma = 0;
+
+		for(int i=0; i<N; i++){
+			x[i] = 0;}
+        do
+		{
+			for(int i=0; i<N; i++)
+			{
+				Y[i] = b[i];
+				for(int j=0; j<N; j++)
+				{
+					if(i != j)
+					{
+						Y[i] -= A[i][j]*x[j];
+					}
+				}
+				Y[i] /= A[i][i];
+			}
+
+		norma = abs(x[0] - Y[0]);
+
+			for(int i=0; i<N; i++)
+			{
+				if(abs(x[i]-Y[i]) > norma)
+					norma = sqrt((x[i]-Y[i])*(x[i]-Y[i]));
+				x[i] = Y[i];
+			}
+		} while (norma >= eps);
+		delete[] Y;
 }
 
-
+void velmiskinaav::MatrVekt(int N, double **A, double *V, double *R)//перемножения матрицы на вектор
+//N- размерность, A- матрица, V- вектор, R- результат
+{
+for(int i=0; i<N; i++)
+        {
+        R[i]=0;
+        for(int j=0; j<N; j++)
+              R[i]+= A[i][j]*V[j];
+        }
+}
 
 /**
  * Метод минимальных невязок
  */
 void velmiskinaav::lab6()
 {
+//N- размерность, A- матрица, b- вектор свободных членов, x-вектор результат
+int count=0;//  количество итераций
+double *R = new double [N];
+double *Delta = new double [N];
+double *TempX = new double[N];
+double *x = new double[N];
+double maxi=0.0, Tau=0.0, TempTau=0.0;
+double eps = 0.0000001;
+for (int i=0; i<N; i++)
+    TempX[i]=0;//первое приближение задаём нулевым
+do
+{
+MatrVekt(N, A, TempX, R);
+for(int i=0; i<N; i++)
+    {
+    Delta[i]=R[i]-b[i];//Вектор невязок
+    }
+MatrVekt(N, A, Delta, R);
+Tau=0.0;
+TempTau=0.0;
+for(int i=0; i<N; i++)
+    {
+    Tau+=R[i]*Delta[i];
+    TempTau+=R[i]*R[i];
+    }
+Tau=Tau/TempTau;
+for(int i=0; i<N; i++)
+    x[i]=TempX[i]-Tau*Delta[i];
+maxi = fabs(x[0] - TempX[0]);
+for(int i=0; i<N; i++)
+    {
+    if(fabs(x[i]-TempX[i])>maxi)
+        maxi=fabs(x[i]-TempX[i]);
+    TempX[i]=x[i];
+    }
+}
+while (maxi>=eps);
 
+delete[] R;
+delete[] Delta;
+delete[] TempX;
 }
 
 
@@ -127,6 +232,39 @@ void velmiskinaav::lab7()
 void velmiskinaav::lab8()
 {
 
+}
+
+//метод касательных
+
+double velmiskinaav::f(double x) {
+
+    return   pow(x,3) + 3*x + 1;
+}
+
+double velmiskinaav::f1(double x) {
+
+    return   3*pow(x, 2)+3;
+}
+
+double velmiskinaav::f2(double x) {
+
+    return  6*x;
+}
+void velmiskinaav::lab9()
+{
+   int a = -3;
+    int b = -2;
+    double c;
+    double eps = 0.0001;
+
+    if(f(a)*f2(a) > 0) c = a;
+    else c = b;
+    do {
+        c=c-f(c)/f1(c);
+
+    }
+    while (fabs(f(c))>=eps);
+        std::cout<<"c="<<c<<"\n";
 }
 
 
