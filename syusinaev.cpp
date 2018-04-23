48c10c165834ab6cdb72cd96709f763f5e4e5f5d
Сюсина Елизавета В <syusinaelv@lab227.local>
2018-04-23 08:59:24 +0300

    4,5,6,10

--- syusinaev.cpp
+++ syusinaev.cpp
@@ -84,39 +84,35 @@ double* Be= new double[N];
  * Метод простых итераций
  */
 void syusinaev::lab4()
-{ 
-double eps=0.001;
-double norma=0;
-double *nx = new double[N];
-
-for (int i=0;i<N;i++){
-x[i]=0;}
+{
+double eps = 1e-9;
+for (int i = 0; i < N; i++) {
+		x[i] = 0;
+	}
+	double x1;
+	double *xr = new double[N];
+	int step = 0;
 	
-do{
-  for(int i=0;i < N;i++)
-  {
-   xn[i]=-b[i];
- 
-   for(int j=0;j < N;j++)
-   {
-    if(i!=j)
-     xn[i]+=A[i][j]*x[j];
-   }
- 
-   xn[i]/=-A[i][i];
-  }
-for(int i=0; i<N; i++) { 
-if(abs(x[i]-xn[i]) > norma)
-norma =abs(x[i]-xn[i]);
+	do {
+		step++;
+		for (int i = 0; i < N; i++) {
+			xr[i] = x[i];
+			for (int k = 0; k < N; k++)
+				xr[i] -= tauh*A[i][k] * x[k];
+			xr[i] += tauh * b[i];
+			
+		}
+		x1 = 0.;
+		for (int i = 0; i < N; i++) {
+			x1 += (xr[i]-x[i])*(xr[i]-x[i]);
+		}
+		
+		for (int i = 0; i < N; i++) {
+			x[i] = xr[i];
+		}
+		printf("err = %f, step = %d\n", x1, step);
+	} while (sqrt(x1)>eps);
 }
-for(int i=0; i<N; i++) 
-	x[i]=xn[i];
-}while (norma>Eps);
-
-delete[] xn;
-
-}
-
 
 
 /**
@@ -153,9 +149,59 @@ double eps=0.001;
 /**
  * Метод минимальных невязок
  */
+ void MatrVekt(int N, double **A, double *V, double *R)//перемножения матрицы на вектор
+//N- размерность, A- матрица, V- вектор, R- результат
+{
+for(int i=0; i<N; i++)
+        {
+        R[i]=0;
+        for(int j=0; j<N; j++)
+              R[i]+= A[i][j]*V[j];
+        }
+}
 void syusinaev::lab6()
 {
+//N- размерность, A- матрица, b- вектор свободных членов, x-вектор результат
+//int count=0;//  количество итераций
+double *R = new double [N];
+double *Delta = new double [N];
+double *TempX = new double[N];
+//double *x = new double[N];
+double maxi=0.0, Tau=0.0, TempTau=0.0;
+double eps = 0.0000001;
+for (int i=0; i<N; i++)
+    TempX[i]=0;//первое приближение задаём нулевым
+do
+{
+MatrVekt(N, A, TempX, R);
+for(int i=0; i<N; i++)
+    {
+    Delta[i]=R[i]-b[i];//Вектор невязок
+    }
+MatrVekt(N, A, Delta, R);
+Tau=0.0;
+TempTau=0.0;
+for(int i=0; i<N; i++)
+    {
+    Tau+=R[i]*Delta[i];
+    TempTau+=R[i]*R[i];
+    }
+Tau=Tau/TempTau;
+for(int i=0; i<N; i++)
+    x[i]=TempX[i]-Tau*Delta[i];
+maxi = fabs(x[0] - TempX[0]);
+for(int i=0; i<N; i++)
+    {
+    if(fabs(x[i]-TempX[i])>maxi)
+        maxi=fabs(x[i]-TempX[i]);
+    TempX[i]=x[i];
+    }
+}
+while (maxi>=eps);
 
+delete[] R;
+delete[] Delta;
+delete[] TempX;
 }
 
 
@@ -183,11 +229,22 @@ void syusinaev::lab9()
 {
 
 }
+static double f(double x) 
+{ 
+return (2*(log(x))-(x/2)); 
+} 
+static double f1(double x) 
+{ 
+return ((2.0/x) - (1.0/2.0)); 
+} 
+static double f2(double x) 
+{ 
+return ((-2.0)/(x*x)); 
+} 
+
 void syusinaev::lab10()
 {
-
 double a=1,b=2 c,e=0.001; 
-
 if( (2*(log(a))-(a/2))*((-2.0)/(a*a))>0 ) c=a; 
 else c=b; 
 do { 
@@ -200,6 +257,13 @@ std::string syusinaev::get_name()
 {
   return "Syusina E.V.";
 }
+
+
+
+
+
+
+
 
 
 
