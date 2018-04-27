f8b22a23f4d402da5da972f24615437b89d12da5
Alexander Mescheryakov <35986721+alexmesch@users.noreply.github.com>
2018-04-27 22:59:53 +0300

    Update mescheryakovam.cpp

--- mescheryakovam.cpp
+++ mescheryakovam.cpp
@@ -154,9 +154,41 @@ void mescheryakovam::lab4()
  * Метод Якоби или Зейделя
  */
 void mescheryakovam::lab5()
-{
+{ 
+//Метод Якоби
+double *oldx = new double[N]; 
 
-}
+for (int i=0; i<N; i++) { 
+x[i]=0; // первоначальное решение 
+} 
+
+double Err=0.0; 
+double eps=1e-20; 
+int k=0; 
+
+do { 
+k++; 
+Err=0.0; 
+for(int i=0; i<N; i++) 
+oldx[i]=x[i]; //предыдущее решение сюда
+for(int i=0; i<N; i++) 
+{ 
+double s=0; //вычисляем s, но не берём диагональные элементы 
+for(int j=0; j<i; j++) 
+s += A[i][j] * oldx[j]; 
+for(int j=i+1; j<N; j++) 
+s += A[i][j] * oldx[j]; 
+x[i]=(b[i] - s)/A[i][i]; // вычисляется новое решение 
+} 
+Err= std::abs(oldx[0]-x[0]); 
+for(int i=0; i<N; i++) 
+{ 
+if(std::abs(oldx[i]-x[i]) > Err) 
+Err = std::abs(oldx[i]-x[i]);//максимальная допустимая разница между предыдущим решением и текущим. 
+} 
+} while(Err >= eps); 
+delete [] oldx; 
+} 
 
 
 
@@ -165,7 +197,66 @@ void mescheryakovam::lab5()
  */
 void mescheryakovam::lab6()
 {
+	
+	double Eps = 1e-18;//погрешность
+	double Del, Res, Abs;//погрешность, невязка, модуль
 
+	double *K = new double[N];//w
+	double *L = new double[N];//v
+	double *xrez = new double[N];
+	
+	
+	//задаём первоначальное приближение
+	for (int i = 0; i<N; i++)
+		xrez[i] = 0;
+
+	//цикл для нахождения корней
+	do{
+		//находим редуцированную систему(одна часть)
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * xrez[j];
+		}
+
+		//находим редуцированную систему(вторая часть)
+		for (int i = 0; i < N; i++) {
+			L[i] = K[i] - b[i];//нахождение вектора невязки
+		}
+
+		
+		//нахождение скалярного произведения матрицы системы и вектора невязки
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * L[j];
+		}
+
+		Res = 0;
+		Abs = 0;
+		
+		//нахождение значения итерационного параметра
+		for (int i = 0; i < N; i++) {
+			Res += K[i] * L[i];
+			Abs += K[i] * K[i];
+		}
+		
+		if (Res==Abs) Res=1;
+		else {
+		Res = Res / Abs;
+		}
+		//получение приближения решения
+		for (int i = 0; i < N; i++)
+			x[i] = xrez[i] - Res*L[i];
+		
+		//Проверка на уменьшение погрешности
+		Del = abs(x[0] - xrez[0]);
+		for (int i = 0; i < N; i++) {
+			if (abs(x[i] - xrez[i])>Del)
+				Del = abs(x[i] - xrez[i]);
+			xrez[i] = x[i];
+		}
+	} while (Eps < Del);
 }
 
 
@@ -175,21 +266,174 @@ void mescheryakovam::lab6()
  */
 void mescheryakovam::lab7()
 {
+	double Eps = 1e-20;//заданная погрешность
+	double Del, s, sAbs;//погрешность итерации, скалярный шаг, модуль шага
 
+
+	double *K = new double[N];
+	double *L = new double[N];
+	double *M = new double[N];
+	double *xrez = new double[N];//итерационные решения
+	
+	
+	//задание начального приближения 
+	for (int i = 0; i<N; i++){
+		xrez[i] = 0;
+	}
+	
+	
+	do {
+		//нахождение скалярного произведения матрицы системы и вектора приближенного решения
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * xrez[j];
+		}
+		
+		//нахождение градиента
+		for (int i = 0; i < N; i++) {
+			L[i] = K[i] - b[i];
+		}
+		
+		//скалярное произведение матрицы системы и градиента
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * L[j];
+		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			M[i] = 0;
+			for (int j = 0; j < N; j++) {
+				M[i] += A[i][j] * K[j];
+			}
+		}
+		
+		s = 0;
+		sAbs = 0;
+		
+		//нахождение величины смещения по направлению градиента(скалярного шага)
+		for (int i = 0; i < N; i++) {
+			s += K[i] * L[i];
+			sAbs += M[i] * K[i];
+		}
+		if (s == sAbs)
+			s = 1;
+		else 
+			s = s / sAbs;
+		//записываем новое приближенное решение
+		for (int i = 0; i < N; i++)
+			x[i] = xrez[i] - s*L[i];
+		
+		//проверка на уменьшение погрешности
+		Del = abs(x[0] - xrez[0]);
+		
+		for (int i = 0; i < N; i++) {
+			if (abs(x[i] - xrez[i])>Del)
+				Del = abs(x[i] - xrez[i]);
+				xrez[i] = x[i];
+		}
+	} while (Eps < Del);
 }
 
+
+/**
+ * Метод вращения для нахождения собственных значений матрицы
+ */
 
 void mescheryakovam::lab8()
 {
 
 }
 
-
+/**
+ * Нахождение наибольшего по модолю собственного значения матрицы
+ */
 void mescheryakovam::lab9()
 {
-
+	double * Y = new double[N];//первый вектор приближения
+	double * M = new double[N];//второй вектор приближения
+	double maxL, L, sum;
+	double EPS = 1e-15;
+	
+	//первичное приближение начального вектора
+	for (int i = 0; i < N; i++)
+		Y[i] = 0;
+	Y[0] = 1;
+	
+	do{
+		sum = 0;
+		//нахождение скалярного произведения векторов приближения 
+		for (int i = 0; i < N; i++)
+			sum += Y[i] * Y[i];
+		
+		L = sqrt(sum);//норма вектора приближения
+		
+		//построение последовательности векторов
+		for (int i = 0; i < N; i++)
+		{
+			M[i] = 0;
+			for (int j = 0; j < N; j++)
+				M[i] += A[i][j] * Y[j] / L;
+		}
+		sum = 0;
+		
+		//сравнение нормы полученного вектора с заданной погрешностью
+		for (int i = 0; i < N; i++)
+			sum += M[i] * M[i];
+		maxL = sqrt(sum);
+		
+		for (int i = 0; i<N; i++)
+			Y[i] = M[i];
+	} while (abs(maxL - L)>EPS);
 }
 
+double static f(double x)
+{
+    double f=x*x*x-2*x*x-5*x+6;
+	return f;
+}
+//производная этой функции 
+double static df(double x)
+{
+    double df=3*x*x-4*x-5;
+	return df;
+}
+
+//сжимающее отображение 
+double static g(double x)
+{
+    return x - f(x)/df(x);
+}
+ 
+
+
+
+void mescheryakovam::lab10()
+{
+    double x;
+    double eps=1e-30;
+    cout<<"Enter initial root value   : ";//начальное приближение корня
+	cin>>x;
+    //итерации приближения
+	for(double iter = 1; eps < fabs(f(x)); iter++){//double,потому что может быть очень много итераций
+        system("cls");
+        
+		cout<<"Iteration : "<<iter<<endl;
+        
+		if(df(x) == 0)//если df(x) == 0, то будет деление на ноль x - f(x)/df(x)
+            break;
+        
+		cout<<"x = "<<x<<endl;
+        cout<<"g(x) = "<<g(x) <<endl;
+        cout<<"df(x)= "<<df(x)<<endl;
+        cout<<"f(x) = "<<f(x) <<endl;
+        
+		x = g(x);//новое приближение
+    }
+    system("pause");
+}
 
 std::string mescheryakovam::get_name()
 {
