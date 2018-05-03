8a35d9004291212f10e7aeefb4d06f2ab372988e
itaevde <35986957+itaevde@users.noreply.github.com>
2018-05-03 13:00:35 +0300

    3 4 5
    ...

--- itaevde.cpp
+++ itaevde.cpp
@@ -67,7 +67,22 @@ double max;
  */
 void itaevde::lab3()
 {
+	int N1=N-1;
+	double *alfa = new double[N];
+	double *beta = new double[N];
+	double y = A[0][0];
+	alfa[0] = -A[0][1] / y;
+	beta[0] = b[0] / y;
+	for (int i = 1; i < N1; i++) {
+		y = A[i][i] + A[i][i - 1] * alfa[i - 1];
+		alfa[i] = -A[i][i + 1] / y;
+		beta[i] = (b[i] - A[i][i - 1] * beta[i - 1]) / y;
+	}
 
+	x[N1] = (b[N1] - A[N1][N1-1] * beta[N1-1]) / (A[N1][N1] + A[N1][N1-1] * alfa[N1-1]);
+	for (int i = N1-1; i >= 0; i--) {
+		x[i] = alfa[i] * x[i + 1] + beta[i];
+	}
 }
 
 
@@ -77,7 +92,61 @@ void itaevde::lab3()
  */
 void itaevde::lab4()
 {
+	double eps = 1e-9;
+	
+	for (int i = 0; i < N; i++) {
+		double maxel = A[i][i];
+		int indRow = i;
+		for (int j = i + 1; j < N; j++)
+			if (maxel < abs(A[j][i])) {
+				indRow = j;
+				maxel = abs(A[j][i]);
+			}
 
+		if (indRow != i) {
+			for (int j = i; j < N; j++) {
+				swap(A[i][j], A[indRow][j]);
+			}
+			swap(b[i], b[indRow]);
+		}
+		
+		double summ = 0;
+		
+		for (int j = 0; j < N; j++)
+			summ += abs(A[i][j]);
+			if (2 * abs(A[i][i]) < summ) {
+                cout << "Error" << std::endl;
+        }
+
+		maxel = A[i][i];
+		b[i] /= A[i][i];
+		A[i][i] = 0;
+		
+		for (int j = 0; j<N; j++)
+			if (j != i)
+                A[i][j] /= maxel;
+
+	}
+	
+	for (int i = 0; i < N; i++) {
+		x[i] = 0;
+	}
+	
+	double xx = b[0];
+	double *results = new double[N];
+	do {
+		for (int i = 0; i < N; i++) {
+			results[i] = 0;
+			for (int k = 0; k < N; k++)
+				results[i] -= A[i][k] * x[k];
+			results[i] += b[i];
+		}
+		xx = x[0];
+		for (int i = 0; i < N; i++) {
+			x[i] = results[i];
+		}
+	} 
+	while (abs(x[0] - xx)>eps);
 }
 
 
@@ -87,7 +156,57 @@ void itaevde::lab4()
  */
 void itaevde::lab5()
 {
+	double eps = 1e-9;    
+	for (int i = 0; i < N; i++) {
+		double maxel = A[i][i];
+		int indRow = i;
+		for (int j = i + 1; j < N; j++)
+			if (maxel < abs(A[j][i])) {
+				indRow = j;
+				maxel = abs(A[j][i]);
+			}
 
+		if (indRow != i) {
+			for (int j = i; j < N; j++) {
+				swap(A[i][j], A[indRow][j]);
+			}
+			swap(b[i], b[indRow]);
+		}
+		
+		double summ = 0;
+		for (int j = 0; j < N; j++)
+			summ += abs(A[i][j]);
+		if (2 * abs(A[i][i]) < summ) {
+                cout << "Error" << std::endl;
+        }
+
+		maxel = A[i][i];
+		b[i] /= A[i][i];
+		A[i][i] = 0;
+		for (int j = 0; j<N; j++)
+			if (j != i)
+                A[i][j] /= maxel;
+	}
+	
+	for (int i = 0; i < N; i++) {
+		x[i] = 0;
+	}
+	
+	double xx = b[0];
+	double *results = new double[N];
+	do {
+		for (int i = 0; i < N; i++) {
+			results[i] = 0;
+			for (int k = 0; k < N; k++)
+				results[i] -= A[i][k] * x[k];
+			results[i] += b[i];
+		}
+		xx = x[0];
+		for (int i = 0; i < N; i++) {
+			x[i] = results[i];
+		}
+	} 
+	while (abs(x[0] - xx)>eps);
 }
 
 
