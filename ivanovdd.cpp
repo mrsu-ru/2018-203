a3594dbf282bd3f0351c4393fb5f8b1097d74dc5
zzdanilivanovzz <daniliv13@mail.ru>
2018-05-08 13:46:08 +0300

    Lab7,8,9

--- ivanovdd.cpp
+++ ivanovdd.cpp
@@ -247,18 +247,157 @@ delete[] temp;
  */
 void ivanovdd::lab7()
 {
+double eps = 1e-5;
+double delta, r, rModul;
 
+double *w = new double[N];
+double *wp = new double[N];
+double *v = new double[N];
+double *result = new double[N];
+
+for (int i = 0; i < N; i++)
+result[i] = 0;
+
+do {
+for (int i = 0; i < N; i++) {
+w[i] = 0;
+for (int j = 0; j < N; j++)
+w[i] += A[i][j] * result[j];
+}
+
+for (int i = 0; i < N; i++) {
+v[i] = w[i] - b[i];
+}
+ 
+for (int i = 0; i < N; i++) {
+w[i] = 0;
+for (int j = 0; j < N; j++)
+w[i] += A[i][j] * v[j];
+}
+
+for (int i = 0; i < N; i++) {
+wp[i] = 0;
+for (int j = 0; j < N; j++) {
+wp[i] += A[i][j] * w[j];
+}
+}
+	
+r = 0.0;
+rModul = 0.0;
+
+for (int i = 0; i < N; i++) {
+r += w[i] * v[i];
+rModul += wp[i] * w[i];
+}
+	
+if (r == rModul) r = 1;
+else r = r / rModul;
+for (int i = 0; i < N; i++)
+x[i] = result[i] - r*v[i];
+delta = abs(x[0] - result[0]);
+for (int i = 0; i < N; i++) {
+if (abs(x[i] - result[i]) > delta)
+delta = abs(x[i] - result[i]);
+result[i] = x[i];
+}
+} while (eps < delta);
 }
 
 
 void ivanovdd::lab8()
 {
+double *sob = new double[N];
+double * b = new double[N];
+double * z = new double[N];
 
+for (int i = 0; i < N; i++){
+z[i] = 0.0;
+b[i] = A[i][i];
+sob[i] = A[i][i];
+}
+
+for (int i = 0; i < 100; i++){
+double sm = 0.0;
+for (int p = 0; p < N - 1; p++){
+for (int q = p + 1; q < N; q++){
+sm += abs(A[p][q]);
+}
+}
+if (sm == 0) break;
+double tresh = sm / (5*N*N);
+for (int p = 0; p < N - 1; p++){
+for (int q = p + 1; q < N; q++){
+double g = 1e12 * abs(A[p][q]);
+if (i >= 3 && abs(sob[p]) > g && abs(sob[q]) > g) A[p][q] = 0.;
+else
+if (abs(A[p][q]) > tresh){
+double theta = (sob[q] - sob[p]) / (2.0 * A[p][q]);
+double t = 1.0 / (abs(theta) + sqrt(1.0 + theta*theta));
+if (theta < 0) t = -t;
+double s = t / sqrt(1.0 + t*t);
+double tau = s / (1.0 + 1.0 / sqrt(1.0 + t*t));
+z[p] -= t * A[p][q];
+z[q] += t * A[p][q];
+sob[p] -= t * A[p][q];
+sob[q] += t * A[p][q];
+A[p][q] = 0.0;
+for (int j = 0; j < p; j++){
+A[j][p] = A[j][p] - s * (A[j][q] + A[j][p] * tau);
+A[j][q] = A[j][q] + s * (A[j][p] - A[j][q] * tau);
+}
+for (int j = p + 1; j < q; j++){
+A[p][j] = A[p][j] - s * (A[j][q] + A[p][j] * tau);
+A[j][q] = A[j][q] + s * (A[p][j] - A[j][q] * tau);
+}
+for (int j = q + 1; j < N; j++){
+A[p][j] = A[p][j] - s * (A[q][j] + A[p][j] * tau);
+A[q][j] = A[q][j] + s * (A[p][j] - A[q][j] * tau);
+}
+}
+}
+}
+for (int p = 0; p < N; p++){
+sob[p] = b[p] + z[p];
+}
+}
+
+for (int p = 0; p < N; ++p) {
+cout << sob[p] << endl;
+}
 }
 
 void ivanovdd::lab9()
 {
+double * Y = new double[N];
+double * y = new double[N];
+double maxSob,sob,sum;
+double eps = 1e-5;
 
+for (int i = 0; i < N; i++)
+Y[i] = 0;
+
+Y[0] = 1;
+
+do{
+sum = 0;
+for (int i = 0; i < N; i++)
+sum += Y[i] * Y[i];
+sob = sqrt(sum);
+for (int i = 0; i < N; i++)
+{
+y[i] = 0;
+for (int j = 0; j < N; j++)
+y[i] += A[i][j] * Y[j] / sob;
+}
+sum = 0;
+for (int i = 0; i < N; i++)
+sum += y[i] * y[i];
+maxSob = sqrt(sum);
+for (int i = 0; i < N; i++)
+Y[i] = y[i];
+} while (abs(maxSob - sob) > eps);
+
+cout << maxSob << endl;
 }
 
 /**
